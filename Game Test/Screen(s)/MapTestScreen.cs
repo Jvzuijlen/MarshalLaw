84a5aabbb9700203474c6f80b0c076e4af44fd2d
Joep van Zuijlen <jvzuijlen@hotmail.com>
2015-12-16 23:47:18 +0100

    Done for tonight, nagedacht/getest hoe we collision gaan detecteren en hoe we sommige tile over de player doen.

--- Game Test/Screen(s)/MapTestScreen.cs
+++ Game Test/Screen(s)/MapTestScreen.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
 
 namespace Game_Test
 {
@@ -12,25 +13,39 @@ namespace Game_Test
     {
         Map map;
 
+        Texture2D Texture;
+        string Path = "SpriteSheets/terrain";
+        Player player;
+
         public MapTestScreen()
         {
 
             map = new Map("testmap2");
+            player = new Player();
         }
 
         public override void LoadContent()
         {
             base.LoadContent();
+
+            content = new ContentManager(ScreenManager.Instance.Content.ServiceProvider, "Content");
+
+                Texture = content.Load<Texture2D>(Path);
+
+            player.LoadContent(0, 0);
         }
 
         public override void UnloadContent()
         {
             base.UnloadContent();
+            player.UnloadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
+
+            player.Update(gameTime);
 
             //When the Escape key has been pressed exit the game
             if (InputManager.Instance.KeyPressed(Keys.Escape))
@@ -42,6 +57,17 @@ namespace Game_Test
         public override void Draw(SpriteBatch spriteBatch)
         {
             base.Draw(spriteBatch);
+            float Scale = 1.0f;
+
+            spriteBatch.Draw(Texture, new Vector2 (0, 0), new Rectangle(756, 288, 32, 32), Color.White, 0, new Vector2(0, 0), Scale, SpriteEffects.None, 0.5f);
+            spriteBatch.Draw(Texture, new Vector2(0, 20), new Rectangle(756, 288, 32, 32), Color.White, 0, new Vector2(0, 0), Scale, SpriteEffects.None, 0.5f);
+            spriteBatch.Draw(Texture, new Vector2(20, 0), new Rectangle(756, 288, 32, 32), Color.White, 0, new Vector2(0, 0), Scale, SpriteEffects.None, 0.5f);
+            spriteBatch.Draw(Texture, new Vector2(20, 20), new Rectangle(756, 288, 32, 32), Color.White, 0, new Vector2(0, 0), Scale, SpriteEffects.None, 0.5f);
+            spriteBatch.Draw(Texture, new Vector2(40, 0), new Rectangle(756, 288, 32, 32), Color.White, 0, new Vector2(0, 0), Scale, SpriteEffects.None, 0.5f);
+            spriteBatch.Draw(Texture, new Vector2(40, 20), new Rectangle(756, 288, 32, 32), Color.White, 0, new Vector2(0, 0), Scale, SpriteEffects.None, 0.5f);
+            spriteBatch.Draw(Texture, new Vector2(40, 20), new Rectangle(0, 0, 32, 32), Color.White, 0, new Vector2(0, 0), Scale, SpriteEffects.None, 0.5f);
+
+            player.Draw(spriteBatch);
         }
     }
 }
