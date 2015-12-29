b83bc4cb7a60379377bc612715587e42af71a1c9
sternold <tobi-pepijn@hotmail.com>
2015-12-29 12:33:13 +0100

    Arduino Port Detection added

---
+++ Game Test/Arduino/ArduinoPortDetection.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System.IO.Ports;
+using System.Text;
+
+namespace Game_Test
+{
+    public class ArduinoPortDetection
+    {
+        List<SerialPort> Ports;
+        SerialPort currentPort;
+        public bool portFound;
+
+        public ArduinoPortDetection()
+        {
+            Ports = new List<SerialPort> { };
+            SetComPort();
+            if (Ports.Count > 0)
+            {
+                portFound = true;
+            }
+        }
+
+        private void SetComPort()
+        {
+            try
+            {
+                string[] ports = SerialPort.GetPortNames();
+                foreach (string port in ports)
+                {
+                    currentPort = new SerialPort(port, 9600);
+                    if (DetectArduino())
+                    {
+                        Ports.Add(currentPort);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        private bool DetectArduino()
+        {
+            try
+            {
+                //The below setting are for the Hello handshake
+                byte[] buffer = new byte[5];
+                buffer[0] = Convert.ToByte(16);
+                buffer[1] = Convert.ToByte(128);
+
+                int intReturnASCII = 0;
+                char charReturnValue = (Char)intReturnASCII;
+
+                currentPort.Open();
+                currentPort.Write(buffer, 0, 1);
+                Thread.Sleep(1000);
+
+                int count = currentPort.BytesToRead;
+                string returnMessage = "";
+                while (count > 0)
+                {
+                    intReturnASCII = currentPort.ReadByte();
+                    returnMessage = returnMessage + Convert.ToChar(intReturnASCII);
+                    count--;
+                }
+                currentPort.Close();
+                if (returnMessage.Contains("HELLO FROM ARDUINO"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public List<SerialPort> ReturnPorts()
+        {
+            return Ports;
+        }
+    }
+}
