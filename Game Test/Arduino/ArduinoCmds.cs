3eea4eaab4b7eb3945a6578d5fa3db155e62df13
sternold <tobi-pepijn@hotmail.com>
2015-12-29 12:46:44 +0100

    Arduino Commands added

---
+++ Game Test/Arduino/ArduinoCmds.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO.Ports;
+using System.Threading;
+using System.Text;
+
+namespace Game_Test
+{
+    class ArduinoCmds
+    {
+        private SerialPort CurrentPort;
+
+        public ArduinoCmds(SerialPort port)
+        {
+            CurrentPort = port;
+        }
+
+        public void Save()
+        {
+            byte[] buffer = new byte[5];
+            buffer[0] = Convert.ToByte(16);
+            buffer[1] = Convert.ToByte(64);
+
+            CurrentPort.Open();
+            CurrentPort.Write(buffer, 0, 1);
+            Thread.Sleep(1000);
+            CurrentPort.Close();
+        }
+    }
+}
