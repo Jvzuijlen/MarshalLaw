edded93c91de80b354410e397ddbf26b5d3ba509
Jvzuijlen <jvzuijlen@hotmail.com>
2015-12-29 14:08:25 +0100

    wewe

--- Game Test/GameManager(s)/ScreenManager.cs
+++ Game Test/GameManager(s)/ScreenManager.cs
@@ -50,7 +50,7 @@ namespace Game_Test
         private ScreenManager()
         {
             //CurrentScreen begint met het SplashScreen
-            currentscreen = new MapTestScreen();
+            currentscreen = new MenuScreen();
             IsTransitioning = false;
         }
 
